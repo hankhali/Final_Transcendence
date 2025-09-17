18585093dc9207ae66eec079be7afa597b4308a5
Mira Taleb <142871330+miraalbareki@users.noreply.github.com>
2025-09-17 17:09:58 +0400

    Update database.js

--- Back-end/queries/database.js
+++ Back-end/queries/database.js
@@ -11,6 +11,7 @@ async function createTables() {
         password TEXT,
         email TEXT UNIQUE,
         avatar TEXT,
+        current_status TEXT DEFAULT 'offline',
         player_matches INTEGER DEFAULT 0,
         player_wins INTEGER DEFAULT 0,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -28,9 +29,9 @@ async function createTables() {
         played_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         round TEXT,
         tournament_id INTEGER,        /* NEW: link match to a tournament */
-        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-        FOREIGN KEY (opponent_id) REFERENCES users (id) ON DELETE CASCADE,
-        FOREIGN KEY (tournament_id) REFERENCES tournaments (id) ON DELETE CASCADE
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL,
+        FOREIGN KEY (opponent_id) REFERENCES users (id) ON DELETE SET NULL,
+        FOREIGN KEY (tournament_id) REFERENCES tournaments (id) ON DELETE SET NULL
       );
 
       CREATE TABLE IF NOT EXISTS tournaments (
@@ -54,12 +55,21 @@ async function createTables() {
         tournament_alias TEXT NOT NULL,
         status TEXT DEFAULT 'joined',
         joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (tournament_id) REFERENCES tournaments (id) ON DELETE CASCADE,
-        FOREIGN KEY (player_id) REFERENCES users (id) ON DELETE CASCADE,
+        FOREIGN KEY (tournament_id) REFERENCES tournaments (id) ON DELETE SET NULL,
+        FOREIGN KEY (player_id) REFERENCES users (id) ON DELETE SET NULL,
         UNIQUE (tournament_id, player_id)
       );
+
+      CREATE TABLE IF NOT EXISTS friends (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER NOT NULL,
+        friend_id INTEGER NOT NULL,
+        friend_request TEXT DEFAULT 'pending', /*'pending', 'accepted', 'rejected'*/
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+        FOREIGN KEY (friend_id) REFERENCES users (id) ON DELETE CASCADE
+      );
     `);
-    //ON DELETE CASCADE: it will automatically remove all game history when a user is deleted
+    //ON DELETE SET NULL: it will automatically remove all game history when a user is deleted
     console.log('Tables ensured');
   } catch (error) {
     console.error('Table setup failed:', error.message);
@@ -72,6 +82,7 @@ createTables();
 
 // Export db so other files can use it
 module.exports = db;
+
 
 
 
